6723e2695d75fb4837a5c745b2330d593fd0223f
Carlos Eduardo Ortega Frias <carlos.ortega17@tectijuana.edu.mx>
2022-05-02 22:00:43 -0700

    Agregar archivos de proyecto.

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace TestDatabase
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(383, 406);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 430);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Test Database";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TestDatabase
+{
+    public partial class FrmMain : Form
+    {
+        private SqlConnection connection;
+
+        private SqlConnection GetConnection()
+        {
+            var connection = new SqlConnection("Data Source=ATROX\\SQLEXPRESS;Initial Catalog=Test;Integrated Security=True");
+            connection.Open();
+            return connection;
+        }
+
+        private void NonQuery(string query)
+        {
+            var command = connection.CreateCommand();
+            command.CommandText = query;
+            command.ExecuteNonQuery();
+        }
+
+        private SqlDataReader Query(string query)
+        {
+            var command = connection.CreateCommand();
+            command.CommandText = query;
+            return command.ExecuteReader();
+        }
+
+        private SqlDataAdapter QueryAdapter(string query)
+        {
+            var command = connection.CreateCommand();
+            command.CommandText = query;
+            return new SqlDataAdapter(command);
+        }
+
+        public FrmMain()
+        {
+            InitializeComponent();
+            connection = GetConnection();
+            var adapter = QueryAdapter("SELECT * FROM dbo.Personas");
+
+            DataSet data = new DataSet();
+            adapter.Fill(data);
+            dataGridView1.DataSource = data.Tables[0];
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            MessageBox.Show(dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString());
+        }
+    }
+}
